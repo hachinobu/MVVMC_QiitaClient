20f64dcec82f44a1bec5700c3fb6acbe8f86a966
hachinobu <hachinobu@gmail.com>
2017-07-19 23:34:47 +0900

    Created TabbarController

---
+++ MVVMC_QiitaClient/Coordinator/Flow/TabbarFlow/TabSelectable.swift
@@ -0,0 +1,16 @@
+//
+//  TabSelectable.swift
+//  MVVMC_QiitaClient
+//
+//  Created by Takahiro Nishinobu on 2017/07/19.
+//  Copyright © 2017年 hachinobu. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+protocol TabSelectable: class {
+    var selectedTimeLineTabObservable: Observable<UINavigationController> { get }
+    var selectedMyAccountTabObservable: Observable<UINavigationController> { get }
+    
+}
---
+++ MVVMC_QiitaClient/View/ViewController/TabbarController.swift
@@ -0,0 +1,53 @@
+//
+//  TabbarController.swift
+//  MVVMC_QiitaClient
+//
+//  Created by Takahiro Nishinobu on 2017/07/19.
+//  Copyright © 2017年 hachinobu. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class TabbarController: UITabBarController, UITabBarControllerDelegate, TabSelectable {
+    
+    private var selectedTimeLineTabObserver = PublishSubject<UINavigationController>()
+    lazy var selectedTimeLineTabObservable: Observable<UINavigationController> =
+        self.selectedTimeLineTabObserver.asObservable()
+    
+    private var selectedMyAccountTabObserver = PublishSubject<UINavigationController>()
+    lazy var selectedMyAccountTabObservable: Observable<UINavigationController> =
+        self.selectedMyAccountTabObserver.asObservable()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        if let viewController = customizableViewControllers?.first {
+            tabBarController(self, didSelect: viewController)
+        }
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        guard let navigationController = viewControllers?[selectedIndex] as? UINavigationController,
+            let selectedTab = SelectedTab(rawValue: selectedIndex) else {
+            return
+        }
+        
+        switch selectedTab {
+        case .timeLine:
+            selectedTimeLineTabObserver.onNext(navigationController)
+        case .myAccount:
+            selectedMyAccountTabObserver.onNext(navigationController)
+        }
+    }
+
+}
+
+fileprivate extension TabbarController {
+    
+    enum SelectedTab: Int {
+        case timeLine = 0
+        case myAccount
+    }
+    
+}
