a8c132f723aa73510bfed8ddc6e482172cd5432c
hachinobu <hachinobu@gmail.com>
2017-08-20 22:40:01 +0900

    WIP Created UserDetailView

---
+++ MVVMC_QiitaClient/View/ViewController/UserDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserDetailViewController.swift
+//  MVVMC_QiitaClient
+//
+//  Created by Takahiro Nishinobu on 2017/08/20.
+//  Copyright © 2017年 hachinobu. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
