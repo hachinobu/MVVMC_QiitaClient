de615c7ff9fbf55f137fc75f539da9ab03be964f
hachinobu <hachinobu@gmail.com>
2017-08-20 22:24:10 +0900

    Created UserDetailViewModel

--- MVVMC_QiitaClient/ViewModel/UserDetailVM.swift
+++ MVVMC_QiitaClient/ViewModel/UserDetailVM.swift
@@ -12,45 +12,109 @@ import RxSwift
 import RxCocoa
 import Action
 
-final class UserDetailVM<UserResult, ItemsResult>: UserDetailViewModel {
+final class UserDetailVM<UserResult, ItemsResult: Sequence>: UserDetailViewModel {
+    
+    typealias UserDetailViewModel = UserDetailTableCellViewModel
+    typealias ItemViewModel = ItemListTableCellViewModel
     
     private let bag = DisposeBag()
+    private var currentPage: Int = 1
     
-    private let userDetailItemPairsObserver = PublishSubject<(UserDetailTableCellViewModel, [ItemListTableCellViewModel])>()
-    lazy var userDetailItemPairs: Driver<(UserDetailTableCellViewModel, [ItemListTableCellViewModel])> =
+    private let userDetailItemPairsObserver = PublishSubject<(UserDetailViewModel, [ItemViewModel])>()
+    lazy var userDetailItemPairs: Driver<(UserDetailViewModel, [ItemViewModel])> =
         self.userDetailItemPairsObserver.asDriver(onErrorDriveWith: .empty())
     
     private let errorObserver = PublishSubject<ActionError>()
     lazy var error: Driver<ActionError> = self.errorObserver.asDriver(onErrorDriveWith: .empty())
     
-    var isLoadingIndicatorAnimation: Driver<Bool> { get }
+    private let isLoadingIndicatorAnimationObserver = PublishSubject<Bool>()
+    lazy var isLoadingIndicatorAnimation: Driver<Bool> = self.isLoadingIndicatorAnimationObserver.asDriver(onErrorJustReturn: false)
+    
     lazy var viewDidLoadTrigger: PublishSubject<Void> = PublishSubject()
     
-    init<UserRequest: QiitaRequest, ItemsRequest: PaginationRequest, Transform: Transformable>(
+    private let userFetchAction: Action<Void, UserDetailViewModel>
+    private let itemFetchAction: Action<Int, [ItemViewModel]>
+    
+    init<UserRequest: QiitaRequest, ItemsRequest: PaginationRequest, UserTransform: Transformable, ItemTransform: Transformable>(
         userRequest: UserRequest,
         itemsRequest: ItemsRequest,
-        transformer: Transform,
+        userTransformer: UserTransform,
+        itemTransformer: ItemTransform,
         session: Session = Session.shared
         ) where UserRequest.Response == UserResult, ItemsRequest.Response == ItemsResult,
-        Transform.Input == (UserResult, ItemsResult),
-        Transform.Output == (UserDetailTableCellViewModel, [ItemListTableCellViewModel]) {
+        UserTransform.Input == UserResult, UserTransform.Output == UserDetailViewModel,
+        ItemTransform.Input == ItemsResult.Iterator.Element, ItemTransform.Output == ItemViewModel {
             
-            let userFetchAction: Action<Void, UserResult> = Action { _ in
-                return session.rx.response(request: userRequest).shareReplayLatestWhileConnected()
+            userFetchAction = Action { _ in
+                return session.rx.response(request: userRequest).map { userTransformer.transform(input: $0) }
             }
             
-//            let itemsFetchAction: Action<Int, ItemsResult> = Action { page in
-//                
-//                var paginationRequest = request
-//                paginationRequest.page = page
-//                
-//                return session.rx.response(request: paginationRequest)
-//                    .map { $0.transform(transformable: transformer) }
-//                    .shareReplayLatestWhileConnected()
-//                
-//            }
+            itemFetchAction = Action { page in
+                var paginationRequest = itemsRequest
+                paginationRequest.page = page
+                return session.rx.response(request: itemsRequest).map { $0.transform(transformable: itemTransformer) }
+            }
+            
+            let fetchItemElements = itemFetchAction.elements.withLatestFrom(itemFetchAction.inputs) { $0 }.scan([ItemViewModel]()) { (elements, result) in
+                let page = result.1
+                let fetchObjects = result.0
+                if page == 1 {
+                    self.currentPage = 1
+                    return fetchObjects
+                }
+                
+                if page > self.currentPage {
+                    self.currentPage = page
+                    return elements + fetchObjects
+                }
+                
+                return elements
+            }
+            
+            Observable.combineLatest(userFetchAction.elements, fetchItemElements)
+                .bind(to: userDetailItemPairsObserver)
+                .addDisposableTo(bag)
+            
+            Observable.amb([userFetchAction.errors, itemFetchAction.errors])
+                .bind(to: errorObserver)
+                .addDisposableTo(bag)
+            
+            viewDidLoadTrigger.subscribe(onNext: { [weak self] in
+                self?.userFetchAction.execute()
+                self?.itemFetchAction.execute(1)
+            }).addDisposableTo(bag)
+            
+            Observable.combineLatest(userFetchAction.executing, itemFetchAction.executing)
+                .map { $0.0 || $0.1 }
+                .bind(to: isLoadingIndicatorAnimationObserver)
+                .addDisposableTo(bag)
             
         
     }
     
+    func bindRefresh(refresh: Driver<Void>) {
+        
+        refresh.asObservable()
+            .withLatestFrom(isLoadingIndicatorAnimation.asObservable())
+            .filter { !$0 }.subscribe(onNext: { [weak self] _ in
+                self?.userFetchAction.execute()
+                self?.itemFetchAction.execute(1)
+            }).addDisposableTo(bag)
+        
+    }
+    
+    func bindReachedBottom(reachedBottom: Driver<Void>) {
+        
+        reachedBottom.asObservable()
+            .withLatestFrom(isLoadingIndicatorAnimation.asObservable())
+            .filter { !$0 }
+            .map { _ in self.currentPage + 1 }
+            .bind(to: itemFetchAction.inputs)
+            .addDisposableTo(bag)
+        
+    }
+    
 }
+
+
+
