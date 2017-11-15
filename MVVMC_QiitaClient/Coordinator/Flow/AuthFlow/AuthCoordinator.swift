bbe267667f753debcd74c5600f55cd9c8055a0e4
hachinobu <hachinobu@gmail.com>
2017-11-15 23:05:28 +0900

    fix typo

--- MVVMC_QiitaClient/Coordinator/Flow/AuthFlow/AuthCoordinator.swift
+++ MVVMC_QiitaClient/Coordinator/Flow/AuthFlow/AuthCoordinator.swift
@@ -35,7 +35,7 @@ final class AuthCoordinator: BaseCoordinator, CoordinatorFinishFlowType {
         } else if status.isLoginFromItem() {
             showLoginAuthViewFromItem()
         } else if status.isLoginFromSignIn() {
-            showLoingAuthViewFromSignIn()
+            showLoginAuthViewFromSignIn()
         }
     }
     
@@ -89,7 +89,7 @@ final class AuthCoordinator: BaseCoordinator, CoordinatorFinishFlowType {
         
     }
     
-    private func showLoingAuthViewFromSignIn() {
+    private func showLoginAuthViewFromSignIn() {
         
         let authView = moduleFactory.generateAuthView()
         authView.skipButtonHidden = true
